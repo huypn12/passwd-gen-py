75606edb0f36b7b40b23514ac8cf4e5b93a23456
Huy Phung <phungnhathuy.92@gmail.com>
2015-03-24 14:41:22 +0700

    Password space generation module completed.

--- agent.py
+++ agent.py
@@ -2,68 +2,85 @@ from poplib import POP3, POP3_SSL
 from imaplib import IMAP4, IMAP4_SSL
 from smtplib import SMTP, SMTP_SSL
 
-def try_auth(server_info, user_info):
-    protocol = server_info['protocol']
-    if (protocol == 'SMTP'):
-        try_auth_smtp(server_info, user_info)
-    elif (protocol == 'POP'):
-        try_auth_pop(server_info, user_info)
-    elif (protocol == 'IMAP'):
-        try_auth_imap(server_info, user_info)
+class EmailAuth(object):
+    @staticmethod
+    def try_auth(server_info, user_info):
+        protocol = server_info['protocol']
+        if (protocol == 'SMTP'):
+            try_auth_smtp(server_info, user_info)
+        elif (protocol == 'POP'):
+            try_auth_pop(server_info, user_info)
+        elif (protocol == 'IMAP'):
+            try_auth_imap(server_info, user_info)
 
-def try_auth_pop(server_info, user_info):
-    try:
-        if (server_info['SSL'] == True):
-            if not server_info['port']:
-                server_info['port'] = 993
-            conn = POP3_SSL(server_info['addr'], server_info['port'])
+    @staticmethod
+    def try_auth_pop(server_info, user_info):
+        try:
+            if (server_info['SSL'] == True):
+                if not server_info['port']:
+                    server_info['port'] = 993
+                conn = POP3_SSL(server_info['addr'], server_info['port'])
+            else:
+                if not server_info['port']:
+                    server_info['port'] = 143
+                conn = POP3(server_info['addr'], server_info['port'])
+            conn.set_debuglevel(2)
+            conn.user(user_info['username'])
+            conn.pass_(user_info['password'])
+        except:
+            print "Invalid credentials:"
         else:
-            if not server_info['port']:
-                server_info['port'] = 143
-            conn = POP3(server_info['addr'], server_info['port'])
-        conn.set_debuglevel(2)
-        conn.user(user_info['username'])
-        conn.pass_(user_info['password'])
-    except:
-        print "Invalid credentials:"
-    else:
-        print "Valid credential"
-    finally:
-        conn.close()
+            print "Valid credential"
+        finally:
+            conn.close()
 
-def try_auth_imap(server_info, user_info):
-    try:
-        if (server_info['SSL'] == True):
-            if not server_info['port']:
-                server_info['port'] = 993
-            conn = IMAP4_SSL(server_info['addr'], server_info['port'])
+    @staticmethod
+    def try_auth_imap(server_info, user_info):
+        try:
+            if (server_info['SSL'] == True):
+                if not server_info['port']:
+                    server_info['port'] = 993
+                conn = IMAP4_SSL(server_info['addr'], server_info['port'])
+            else:
+                if not server_info['port']:
+                    server_info['port'] = 143
+                conn = IMAP4(server_info['addr'], server_info['port'])
+            conn.set_debuglevel(2)
+            conn.user(user_info['username'])
+            conn.pass_(user_info['password'])
+        except:
+            print "Invalid credentials:"
         else:
-            if not server_info['port']:
-                server_info['port'] = 143
-            conn = IMAP4(server_info['addr'], server_info['port'])
-        conn.set_debuglevel(2)
-        conn.user(user_info['username'])
-        conn.pass_(user_info['password'])
-    except:
-        print "Invalid credentials:"
-    else:
-        print "Valid credential"
+            print "Valid credential"
 
-def try_auth_smtp(server_info, user_info):
-    try:
-        if (server_info['SSL'] == True):
-            if not server_info['port']:
-                server_info['port'] = 993
-            conn = SMTP_SSL(server_info['addr'], server_info['port'])
+    @staticmethod
+    def try_auth_smtp(server_info, user_info):
+        try:
+            if (server_info['SSL'] == True):
+                if not server_info['port']:
+                    server_info['port'] = 993
+                conn = SMTP_SSL(server_info['addr'], server_info['port'])
+            else:
+                if not server_info['port']:
+                    server_info['port'] = 143
+                conn = SMTP(server_info['addr'], server_info['port'])
+            conn.set_debuglevel(2)
+            conn.login(user_info['username'], user_info['password'])
+        except:
+            print "Invalid credentials:"
         else:
-            if not server_info['port']:
-                server_info['port'] = 143
-            conn = SMTP(server_info['addr'], server_info['port'])
-        conn.set_debuglevel(2)
-        conn.login(user_info['username'], user_info['password'])
-    except:
-        print "Invalid credentials:"
-    else:
-        print "Valid credential"
+            print "Valid credential"
 
+if __name__ == '__main__':
+    server_info = {
+        'protocol': 'POP',
+        'SSL': True,
+        'addr': 'pop3.google.com',
+        'port': '993'
+    }
+    user_info = {
+        'username': 'hoanglatk26',
+        'password': 'abc'
+    }
 
+    EmailAuth.try_auth(server_info, user_info)
--- genpass.py
+++ genpass.py
@@ -1,26 +1,130 @@
+import re
+import itertools
 
 # Note1: Password generation structure
-
 #--------------------------------------------------------------
-# Token list:
-# Terminal character w
-# Dictionary representation $
-# Pattern representation %
+def nested_item(depth, value):
+    if depth <= 1:
+        return [value]
+    else:
+        return [nested_item(depth - 1, value)]
+
+def nested_list(n):
+    """Generate a nested list where the i'th item is at depth i."""
+    lis = []
+    for i in range(n):
+        if i == 0:
+            lis.append(i)
+        else:
+            lis.append(nested_item(i, i))
+    return lis
+
+def flatten(lis):
+    """Given a list, possibly nested to any level, return it flattened."""
+    new_lis = []
+    for item in lis:
+        if type(item) == type([]):
+            new_lis.extend(flatten(item))
+        else:
+            new_lis.append(item)
+    return new_lis
+
+
 class PasswordSpace(object):
-    NAME = ['FIRST', 'LAST', 'MIDDLE']
-    DATE = ['DAY', 'MONTH', 'YEAR']
-    # Predefined pattern
-    #
+    # Preserved dictionary keywords, userinfo must implement this
+    DICT_NAMES = [
+        'NAME',
+        'DOB',
+        'TEL'
+    ]
+    DICT_KEYS = [
+        # Full name
+        'FIRST',
+        'LAST',
+        'MIDDLE',
+        # DoB
+        'DD',
+        'MM',
+        'YY',
+        'YYYY',
+        # Tel
+        'MOBILE',
+        'WORK'
+    ]
+
+    # Predefined pattern, applied for all agent
+    PP = {
+        'DATE_0': "${DD}${MM}${YYYY}",
+        'DATE_1': "${MM}${DD}${YYYY}",
+        'NAME_0': "${FIRST}${LAST}",
+        'NAME_1': "${FIRST}${MIDDLE}${LAST}"
+    }
+
+    # Password structure
+    P = {
+        'P1': "%{NAME_0}%{DATE_0}",
+        'P2': "#{1234}#{___}%{DATE_0}"
+    }
     def __init__(self, account_info):
         self.account_info = account_info
-        pass
+        self.password_space = []
 
-    def tokenizer(string):
+    @staticmethod
+    def parse(string):
+        element_list = re.findall(r"(.*?)\{(\w+)\}", string)
+        return element_list
 
-        pass
+    @staticmethod
+    def stdpat(string):
+        structure = PasswordSpace.parse(string)
+        enumerated_structure = (i for i,x in enumerate(structure) if x[0] == '%')
+        for i in enumerated_structure:
+            pattern_str = PasswordSpace.PP[structure[i][1]]
+            pattern = PasswordSpace.parse(pattern_str)
+            del structure[i]
+            structure[i:i] = pattern
+        return structure
 
-    def genpws():
-        pass
+    def genpws(self):
+        for p in PasswordSpace.P.items():
+
+            structure = PasswordSpace.stdpat(p[1])
+
+            element_list = []
+            for element in structure:
+                tmp = []
+                if element[0] == '#':
+                    tmp.append(element[1])
+                    element_list.append(tmp)
+                elif element[0] == '$':
+                    element_list.append(self.account_info[element[1]])
+
+            product_list = []
+            for dict_elem in element_list:
+                if product_list == []:
+                    product_list = dict_elem
+                    continue
+                else:
+                    product_list = [list(item) for item in itertools.product(product_list, dict_elem)]
+
+            password_list = []
+            for nested_product in product_list:
+                flat_product = flatten(nested_product)
+                password = ''.join(flat_product)
+                password_list.append(password)
+
+        self.password_space = password_list
+
+
+    def numpws(self, s):
+        structure = PasswordSpace.stdpat(s)
+        pws_size = 0
+        for element in structure:
+            if element[0] == '#':
+                pws_size += 1
+            elif element[0] == '$':
+                pws_size += len(self.account_info[element[1]])
+        return pws_size
 
     def genpws_pat_partial():
         pass
@@ -28,15 +132,23 @@ class PasswordSpace(object):
     def genpws_partial():
         pass
 
-    def stdpat():
-        pass
 
-    def numpws():
-        pass
-
-# genpws: generate password space from a dictionary data set, a password structure set,
-# @param: List P: password structure
-# @param: Dictionary S: dictionary data set
-# @param:
-# @return
-
+if __name__ == '__main__':
+    account_info = {
+        # Full name
+        'FIRST': ['la', 'dung'],
+        'LAST': ['nguyen', 'hoang'],
+        'MIDDLE': ['thi', 'van'],
+        # DoB
+        'DD': ['2', '02'],
+        'MM': ['10', '07'],
+        'YYYY': ['1982', '82'],
+        # Tel
+        'MOBILE': ['xxxx'],
+        'WORK': ['yyyy']
+    }
+    password_space = PasswordSpace(account_info)
+    #PasswordSpace.stdpat(PasswordSpace.P['P2'])
+    password_space.genpws()
+    for password in password_space.password_space:
+        print password
